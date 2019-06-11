7db0963fdb3d31f1156063e762202f6114feca21
hu243285237 <243285237@qq.com>
2019-06-11 11:52:44 +0800

    更新笔击

--- 代码笔记/javascript/log.js
+++ 代码笔记/javascript/log.js
@@ -3,3 +3,10 @@ document.querySelector('.example')
 
 // parseInt 可解析一个字符串并返回一个整数
 parseInt('24')
+
+// setInterval() 方法会不停地调用函数，
+// 直到 clearInterval() 被调用或窗口被关闭
+// 第一个参数为执行的函数，第二个参数为周期（毫秒）
+setInterval(() => {
+    console.log('abc')
+}, 2000)
