a5440d94e55378283db07025df04ed38cb244a74
hu243285237 <243285237@qq.com>
2019-06-14 17:52:44 +0800

    更新对象介绍

--- 代码笔记/javascript/普通对象与函数对象/index.js
+++ 代码笔记/javascript/普通对象与函数对象/index.js
@@ -1,48 +1,24 @@
 /*
-    js对象分为普通对象和函数对象
-
-    Object、Function是js自带的函数对象
-
-    凡是通过new Function创建的对象都是函数对象，其他都是普通对象
-
-    f1、f2归根结底都是通过new Function()的方式进行创建的
-
-    Function、Object也都是通过new Function()创建的
+    对象是通过函数创建的，而函数又是一种对象
 */
 
-var o1 = {};
-var o2 = new Object();
-var o3 = new f1();
+var o1 = {}; // 使用{}其实就是new Object的一种语法糖
+var o2 = new Object(); // 通过函数创建对象
+var o3 = new f1(); // f1在这里是构造函数
 
-function f1() { };
-var f2 = function () { };
-var f3 = new Function('str', 'console.log(str)');
+function f1() {};
+var f2 = function () {};
+var f3 = new Function();
 
-console.log(typeof Object); // function
-console.log(typeof Function); // function
+
+
+console.log(o1); // object
+console.log(o2); // object
+console.log(typeof o3); // object
 
 console.log(typeof f1); // function
 console.log(typeof f2); // function
 console.log(typeof f3); // function
 
-console.log(typeof o1); // object
-console.log(typeof o2); // object
-console.log(typeof o3); // object
-
-
-
-// 创建函数的三种方式（推荐第二种）：
-
-function sum1(a, b) {
-    return a + b;
-}
-
-var sum2 = function (a, b) {
-    return a + b;
-}
-
-var sum3 = new Function('a', 'b', 'return a + b;')
-
-console.log(typeof sum1); // function
-console.log(typeof sum2); // function
-console.log(typeof sum3); // function
+console.log(typeof Object); // function
+console.log(typeof Function); // function
