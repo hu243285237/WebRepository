85183afc8ec4e6dfb0be5ea66f6d7120472baa8b
hu243285237 <243285237@qq.com>
2019-07-24 10:33:00 +0800

    添加zcg给的proxy题目2

--- 代码笔记/javascript/Proxy/index.js
+++ 代码笔记/javascript/Proxy/index.js
@@ -47,25 +47,68 @@ pro.age = 18;
 // "63" | 0 = 63
 var pipe = (function () {
     return function (value) {
-      var funcStack = [];
-      var oproxy = new Proxy({} , {
-        get : function (pipeObject, fnName) {
-          if (fnName === 'get') {
-            return funcStack.reduce(function (val, fn) {
-              return fn(val);
-            },value);
-          }
-          funcStack.push(window[fnName]);
-          return oproxy;
-        }
-      });
-  
-      return oproxy;
+        var funcStack = [];
+        var oproxy = new Proxy({}, {
+            get: function (pipeObject, fnName) {
+                if (fnName === 'get') {
+                    return funcStack.reduce(function (val, fn) {
+                        return fn(val);
+                    }, value);
+                }
+                funcStack.push(window[fnName]);
+                return oproxy;
+            }
+        });
+
+        return oproxy;
     }
-  }());
-  
-  var double = n => n * 2;
-  var pow    = n => n * n;
-  var reverseInt = n => n.toString().split("").reverse().join("") | 0;
-  
-  pipe(3).double.pow.reverseInt.get; // 63
+}());
+
+var double = n => n * 2;
+var pow = n => n * n;
+var reverseInt = n => n.toString().split("").reverse().join("") | 0;
+
+console.log(pipe(3).double.pow.reverseInt.get); // 63
+
+//----------------------------------------------------------------------
+
+// zcg 给的题目2
+// 关键点：
+// dom 是一个 proxy 对象
+// 当 dom.div 时执行 get 方法
+// get 方法返回的是一个函数，所以 dom.div 就可以当作函数来用
+// 第一个参数是这个标签的属性值，后面的参数是这个标签的子标签
+// 第一个 for 循环把属性值全部赋予这个标签
+// 第二个 for 循环把子标签添加到这个父标签上
+const dom = new Proxy({}, {
+    get(target, property) {
+        return function (attrs = {}, ...children) {
+            const el = document.createElement(property);
+            for (let prop of Object.keys(attrs)) {
+                el.setAttribute(prop, attrs[prop]);
+            }
+            for (let child of children) {
+                if (typeof child === 'string') {
+                    child = document.createTextNode(child);
+                }
+                el.appendChild(child);
+            }
+            return el;
+        }
+    }
+});
+
+const el = dom.div({},
+    'Hello, my name is ',
+    dom.a({ href: '//example.com' }, 'Mark'),
+    '. I like:',
+    dom.ul({},
+        dom.li({}, 'The web'),
+        dom.li({}, 'Food'),
+        dom.li({}, '…actually that\'s it')
+    )
+);
+
+document.body.appendChild(el);
+
+//----------------------------------------------------------------------
