9bb32cac738cb32736bc886d3a9932a052f175d6
hu243285237 <243285237@qq.com>
2019-08-10 15:26:12 +0800

    添加Watcher

--- 代码笔记/javascript/双向绑定/index.js
+++ 代码笔记/javascript/双向绑定/index.js
@@ -65,6 +65,8 @@ function defineReactive (data, key, value) {
     });
 }
 
+//-----------------------------------------------------
+
 // 容器 Dep 做统一管理
 function Dep() {
     this.subs = [];
@@ -87,4 +89,77 @@ var people02 = {
 observer(people02);
 people02.name = "Youououo";
 
+//-----------------------------------------------------
+
+// 2. 订阅者 Watcher
+
+function Watcher(vm, prop, callback) {
+    this.vm = vm;
+    this.prop = prop;
+    this.callback = callback;
+    this.value = this.get();
+}
+Watcher.prototype.update = function () {
+    const value = this.vm.$data[this.prop];
+    const oldVal = this.value;
+    if (value !== oldVal) {
+        this.value = value;
+        this.callback(value);
+    }
+}
+Watcher.prototype.get = function () {
+    Dep.target = this; // 储存订阅器
+    const value = this.vm.$data[this.prop]; // 因为属性被监听，这一步会执行监听器里的 get 方法
+    Dep.target = null;
+    return value;
+}
+
+//-----------------------------------------------------
+
+// 测试
+function mVue(options, prop) {
+    this.$options = options;
+    this.$data = options.data;
+    this.$prop = prop;
+    this.$el = document.querySelector(options.el);
+    this.init();
+}
+mVue.prototype.init = function () {
+    observer(this.$data);
+    this.$el.textContent = this.$data[this.$prop];
+    new Watcher(this, this.$prop, value => {
+        this.$el.textContent = value;
+    });
+}
+
+const vm = new mVue({
+    el: "#app",
+    data: {
+        name: "HWJ"
+    }
+}, "name");
+
+//-----------------------------------------------------
+/*
+// 3. 解析器 Compile
+
+function Compile (vm) {
+    this.vm = vm;
+    this.el = vm.$el;
+    this.fragment = null;
+    this.init();
+}
+Compile.prototype.init = function () {
+    this.fragment = this.nodeFragment(this.el);
+}
+Compile.prototype.nodeFragment = function (el) {
+    const fragment = document.createDocumentFragment();
+    let child = el.firstChild;
+    while (child) { // 将子节点，全部移动文档片段里
+        fragment.appendChild(child);
+        child = el.firstChild;
+    }
+    return fragment;
+}
+*/
 //-----------------------------------------------------
