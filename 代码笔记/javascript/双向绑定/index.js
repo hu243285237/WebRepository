364ccb9c482dc38a2691e579e090f55184605f0d
hu243285237 <243285237@qq.com>
2019-08-12 19:26:08 +0800

    更新双向绑定

--- 代码笔记/javascript/双向绑定/index.js
+++ 代码笔记/javascript/双向绑定/index.js
@@ -10,23 +10,6 @@
 
 //-----------------------------------------------------
 
-
-var people01 = {};
-
-Object.defineProperty (people01, "name", {
-    get: function () {
-        console.log("Get name");
-        return name;
-    },
-    set: function (newVal) {
-        console.log("Set name");
-        name = newVal;
-    }
-});
-
-people01.name; // "Get name"
-people01.name = "Yohehehe"; // "Set name"
-
 // Observer 监听器：用来监听属性的变化通知订阅者
 //
 // Watcher 订阅者：收到属性的变化，然后更新视图
@@ -41,15 +24,17 @@ function observer (data) {
     if (!data || typeof data !== "object") {
         return;
     }
-    Object.keys(data).forEach(key => {
+    Object.keys(data).forEach(key => { // 取出所有属性遍历
         defineReactive(data, key, data[key]);
     });
 }
 
 function defineReactive (data, key, value) {
-    observer(value); // 递归调用，监听所有属性
+    observer(value); // 递归调用，监听子属性
     var dep = new Dep();
     Object.defineProperty(data, key, {
+        enumerable: true, // 可枚举
+        configurable: false, // 不能再 define
         get: function () {
             if (Dep.target) {
                 dep.addSub(Dep.target);
@@ -139,53 +124,76 @@ const vm = new mVue({
 
 // 3. 解析器 Compile
 
-function Compile (vm) {
-    this.vm = vm;
-    this.el = vm.$el;
-    this.fragment = null;
-    this.init();
+function Compile (el) {
+    this.$el = this.isElementNode(el) ? el : document.querySelector(el);
+    if (this.$el) {
+        this.$fragment = this.node2Fragment(this.$el);
+        this.init();
+        this.$el.appendChild(this.$fragment);
+    }
 }
 Compile.prototype.init = function () {
-    this.fragment = this.nodeFragment(this.el);
+    this.compileElement(this.$fragment);
 }
-Compile.prototype.nodeFragment = function (el) {
-    const fragment = document.createDocumentFragment();
+Compile.prototype.node2Fragment = function (el) {
+    const fragment = document.createDocumentFragment(), child;
     let child = el.firstChild;
-    while (child) { // 将子节点，全部移动文档片段里
+    while (child) { // 将子节点，全部移动到 fragment
         fragment.appendChild(child);
         child = el.firstChild;
     }
     return fragment;
 }
-Compile.prototype.compileNode = function (fragment) {
-    let childNodes = this.fragment.childNodes;
-    [...childNodes].forEach(node => {
-        let reg = /\{(.*)\}/;
+Compile.prototype.compileElement = function (el) {
+    let childNodes = el.childNodes, me = this;
+    [].slice.call(childNodes).forEach(node => {
+        let reg = /\{\{(.*)\}\}/;
         let text = node.textContent;
-        if (this.isElementNode(node)) {
-            this.compileNode(node); // 渲染指令模板
+        if (me.isElementNode(node)) {
+            me.compile(node); // 渲染指令模板
         }
-        else if (this.isTextNode(node) && reg.test(text)) {
-            let prop = RegExp.$1;
-            this.compileText(node, prop); // 渲染 {{}} 模板
+        else if (me.isTextNode(node) && reg.test(text)) {
+            me.compileText(node, RegExp.$1); // 渲染 {{ }} 模板
         }
-
-        if (node.childNodes && node.childNodes.length) {
-            this.compileNode(node);
+        if (node.childNodes && node.childNodes.length) { // 遍历编译子节点
+            me.compileElement(node);
         }
     });
 }
-Compile.prototype.compile = function () {
-    let nodeAttrs = node.attributes;
-    [...nodeAttrs].forEach(attr => {
-        let name = attr.name;
-        if (this.isDirective(name)) {
-            let value = attr.value;
-            if (name === "v-model") {
-                this.compileModel(node, value);
+Compile.prototype.compile = function (node) {
+    let nodeAttrs = node.attributes, me = this;
+    [].slice.call(nodeAttrs).forEach(attr => {
+        let attrName = attr.name;
+        if (this.isDirective(attrName)) {
+            let exp = attr.value; // content
+            var dir = attrName.substring(2); // text
+            if (me.isEventDirective(dir)) { // 事件指令，如 v-on:click
+                compileUtil.eventHandler(node, me.$vm, exp, dir);
+            } else { // 普通指令
+                compileUtil[dir] && compileUtil[dir](node, me.$vm, exp);
             }
         }
     });
+}
+
+// 指令处理集合
+var compileUtil = {
+    text: function (node, vm, exp) {
+        this.bind(node, vm, exp, 'text');
+    },
+    bind: function (node, vm, exp, dir) {
+        var updaterFn = updater[dir + 'Updater'];
+        updaterFn && updaterFn(node, vm[exp]); // 第一次初始化视图
+        new Watcher(vm, exp, function (value, oldValue) { // 实例化订阅者，此操作会在对应的属性消息订阅器中添加该订阅者 watcher
+            updaterFn && updaterFn(node, value, oldValue); // 一旦属性值有变化，会收到通知执行此更新函数，更新视图
+        });
+    }
+}
+
+var updater = {
+    textUpdater: function (node, value) {
+        node.textContent = typeof value == 'undefined' ? '' : value;
+    }
 }
 
 //-----------------------------------------------------
