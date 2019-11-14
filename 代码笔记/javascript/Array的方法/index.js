1e5f634ef2438d9a2200d8a8f24e556428c2547e
hu243285237 <243285237@qq.com>
2019-11-14 18:36:39 +0800

    添加isArray和of方法介绍

--- 代码笔记/javascript/Array的方法/index.js
+++ 代码笔记/javascript/Array的方法/index.js
@@ -8,3 +8,29 @@ var a1 = "123";
 console.log(Array.from(a1)); // ['1', '2', '3']
 console.log(Array.from(a1, x => x * 5 )); // [5, 10, 15]
 console.log(a1); // "123"
+
+//-------------------------------------------------------------
+
+// Array.isArray(obj)
+// 检测一个值是否是数组类型，返回一个布尔值
+var a2 = [3, 5, 6];
+var b2 = "888";
+var c2 = {};
+console.log(Array.isArray(a2)); // true
+console.log(Array.isArray(b2)); // false
+console.log(Array.isArray(c2)); // false
+console.log(Array.isArray(Array.prototype)); // true
+console.log(Array.isArray(new Uint8Array(32))); // false
+
+//-------------------------------------------------------------
+
+// Array.of(element0[, element1[, ...[, elementN]]])
+// 此方法创建一个具有可变数量参数的新数组实例，而不考虑参数的数量或类型
+// 参数 elementN，任意个参数，将按顺序成为数组中的元素
+// Array.of 和 Array 构造函数之间的区别在于处理整数参数
+// Array.of(7) 创建一个具有单个元素 7 的数组
+// 而 Array(7) 创建一个长度为 7 的空数组（这里是指有 7 个空位，即 [,,,,,,,]，而不是 7 个 undefined）
+console.log(Array(7)); // [,,,,,,,]
+console.log(Array(1, 2, 3)); // [1, 2, 3]
+console.log(Array.of(7)); // [7]
+console.log(Array.of(1, 2, 3)); // [1, 2, 3]
