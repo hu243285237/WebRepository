4545384a9a3708c645d5014cda784e7c8128a198
hu243285237 <243285237@qq.com>
2019-11-18 13:57:54 +0800

    添加洗牌算法

---
+++ 代码笔记/javascript/数组乱序/index.js
@@ -0,0 +1,25 @@
+///////////////////////////////////////////////
+//
+//  著名的洗牌算法 Fisher-Yates shuffle
+//
+//  Fisher-Yates 费雪 耶兹 两个人一起提出的
+//
+//  shuffle 的意思是洗牌
+//
+//  时间复杂度 O(n)
+//
+//  空间复杂度 O(1)
+//
+///////////////////////////////////////////////
+
+function shuffle(arr) {
+    let m = arr.length;
+    while (m > 1) {
+        let index = Math.floor(Math.random() * m--);
+        [arr[index], arr[m]] = [arr[m], arr[index]];
+    }
+    return arr;
+}
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(shuffle(arr));
