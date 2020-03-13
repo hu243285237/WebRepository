0bd81264ebb95d94b16f47c6539b0956c9051ab3
hu243285237 <243285237@qq.com>
2020-03-13 16:44:46 +0800

    解决无法解析子节点的bug

--- 代码笔记/vue/Vue双向绑定原理/compile.js
+++ 代码笔记/vue/Vue双向绑定原理/compile.js
@@ -1,8 +1,10 @@
 
 // 将目标节点下的子节点都添加进 fragment
 function nodeToFragment(node, vm) {
+    // 创建 fragment（轻量级的 document 对象）
     let fragment = document.createDocumentFragment();
     let child = node.firstChild;
+    // 遍历根节点下的所有一级节点
     while (child) {
         compile(child, vm);
         fragment.appendChild(child);
@@ -12,8 +14,20 @@ function nodeToFragment(node, vm) {
 }
 
 function compile(node, vm) {
-    // 如果是元素节点（input 标签）
+    // 如果是元素节点，比如 input、div、p 标签等
     if (node.nodeType === 1) {
+        compileElement(node, vm);
+    }
+    // 如果元素是文本节点（比如"<div>{{ xxx }}</div> 的 {{ xxx }}"）
+    else if (node.nodeType === 3) {
+        compileText(node, vm);
+    }
+}
+
+// 解析元素节点
+function compileElement(node, vm) {
+    // 如果是 input 标签
+    if (node.tagName === 'INPUT') {
         // 解析这个元素的所有属性
         for (let attr of node.attributes) {
             // 如果这个属性是 v-model
@@ -32,20 +46,28 @@ function compile(node, vm) {
             }
         }
     }
-    // 如果元素是文本节点（无标签，比如"<div></div>{{ message }}"）
-    else if (node.nodeType === 3) {
-        // 来匹配 {{ xxx }} 中的 xxx
-        let reg = /\{\{(.*)\}\}/;
-        // 如果这个文本里有 {{ xxx }} 这样的文本
-        if (reg.test(node.nodeValue)) {
-            // 获取匹配到的第一个字符串
-            let name = RegExp.$1;
-            // 去掉字符串的首尾空格
-            name = name.trim();
-            // 初始化文本节点的值
-            node.nodeValue = vm[name];
-            // 绑定一个订阅者
-            new Watcher(vm, node, name);
+    // 如果是其他标签，比如 div、p 等
+    else {
+        // 编译他们的子节点
+        for (let child of node.childNodes) {
+            compile(child, vm);
         }
+    }
+}
+
+// 解析文本节点
+function compileText(node, vm) {
+    // 来匹配 {{ xxx }} 中的 xxx
+    let reg = /\{\{(.*)\}\}/;
+    // 如果这个文本里有 {{ xxx }} 这样的文本
+    if (reg.test(node.nodeValue)) {
+        // 获取匹配到的第一个字符串
+        let name = RegExp.$1;
+        // 去掉字符串的首尾空格
+        name = name.trim();
+        // 初始化文本节点的值
+        node.nodeValue = vm[name];
+        // 绑定一个订阅者
+        new Watcher(vm, node, name);
     }
 }
--- 代码笔记/vue/Vue双向绑定原理/vue.js
+++ 代码笔记/vue/Vue双向绑定原理/vue.js
@@ -4,7 +4,7 @@ function Vue(options) {
     // 使 data 里的属性都变为可侦测的
     observable(options.data, this);
     // 得到根元素，一般都是 <div id="app"></div>
-    let node = document.getElementById(options.el);
+    let node = document.querySelector(options.el);
     // 将根元素下的节点都移到 fragment 去编译，就是查找 v-model 和 {{ }}
     let fragment = nodeToFragment(node, this);
     // 再将这个 fragment 重新挂到根元素上
